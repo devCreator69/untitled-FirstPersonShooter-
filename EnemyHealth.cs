3fc5a35beab38ea2c8f2c10ad24758f2e103e8bb
devCreator69 <81888953+devCreator69@users.noreply.github.com>
2021-10-01 13:53:54 -0400

    Update EnemyHealth.cs

--- EnemyHealth.cs
+++ EnemyHealth.cs
@@ -1,33 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyHealth : MonoBehaviour
 {
-    [SerializeField] float hitPoints = 100f;
+       [SerializeField] float hitPoints = 100f;
 
     public AudioSource audioSource;
     public AudioClip alarm;
 
-   
     bool alreadyPlayed = false;
-
-    bool isDead = false;
+    public bool isDead = false;
     public bool IsDead() { return isDead; }
 
     public void TakeDamage(float damage)
     {
+        if(gameObject.tag == "Nonhostile")
+        {
+            PlayAlarm();
+        }
+
         // BroadcastMessage can call a method that is on the game object or any of the game objects children
         BroadcastMessage("OnDamageTaken");
         hitPoints -= damage;
+        
+
         if (hitPoints <= 0)
         {
             Die();
-            //Destroy(gameObject); 
         }
         
     }
-
     private void PlayAlarm () 
     {
         if (!alreadyPlayed)
@@ -36,24 +40,11 @@ public class EnemyHealth : MonoBehaviour
             alreadyPlayed = true;
         }
     }
- 
     private void Die()
     {
-        if(gameObject.tag == "Nonhostile")
-        {
-            Debug.Log("I was Nonhostile");
-            PlayAlarm();
-
-            if (isDead) return;
-            isDead = true;
-            GetComponent<Animator>().SetTrigger("die");
-        }
-        else
-        {
-            // will only allow die animation to play once
-            if (isDead) return;
-            isDead = true;
-            GetComponent<Animator>().SetTrigger("die"); 
-        }
+        // will only allow die animation to play once
+        if (isDead) return;
+        isDead = true;
+        GetComponent<Animator>().SetTrigger("die"); 
     }
 }
